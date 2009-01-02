43b16443968bfdef871adde7db6c5299ccc008cf
Alistair Kearney <alistair@pointybeard.com>
2009-01-02 13:38:27 +1100

    Data Source implementation. Is dependent on the ASDC library (git://github.com/pointybeard/asdc.git)

--- data-sources/data.breadcrumb.php
+++ data-sources/data.breadcrumb.php
@@ -1,11 +1,14 @@
 <?php
 
 	require_once(TOOLKIT . '/class.datasource.php');
+	require_once(EXTENSIONS . '/asdc/class.asdc.php');
 	
 	Class datasourceBreadcrumb extends Datasource{
 		
 		public $dsParamROOTELEMENT = 'breadcrumb';
-
+		
+		private $_database;
+		
 		public function about(){
 			return array(
 					 'name' => 'Breadcrumb',
@@ -17,13 +20,59 @@
 					 'release-date' => '2009-01-02');	
 		}
 
-		
 		public function grab(&$param_pool){
 			$result = new XMLElement($this->dsParamROOTELEMENT);
 			
-			## TODO
+			$current_page_id = (int)$this->_env['param']['current-page-id'];
+			
+			$db = ASDCLoader::instance();
+			
+			try{
+				$results = $db->query("SELECT * FROM `tbl_pages` WHERE `id` = '{$current_page_id}' LIMIT 1");
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', General::sanitize(vsprintf('%d: %s on query "%s"', $db->lastError()))));
+				return $result;
+			}
+
+			
+			/*
+			stdClass Object
+			(
+			    [id] => 82
+			    [parent] => 81
+			    [title] => J.J. Goldberg
+			    [handle] => jj-goldberg
+			    [path] => about/masthead
+			    [params] => 
+			    [data_sources] => breadcrumb
+			    [events] => 
+			    [sortorder] => 13
+			)
+			
+			<breadcrumb>
+				<page path="about">About</page>
+				<page path="about/contact">Contact</page>
+				<page path="about/contact/office">Forward Office</page>
+			</breadcrumb>			
+			
+			*/
+			
+			
+			while($results->length() > 0){
 				
-			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+				$current = $results->current();
+				
+				$result->prependChild(
+					new XMLElement('page', $current->title, array('path' => trim("{$current->path}/{$current->handle}", '/')))
+				);
+				
+				if(is_null($current->parent)) break;
+
+				$results = $db->query(sprintf("SELECT * FROM `tbl_pages` WHERE `id` = '%d' LIMIT 1", (int)$current->parent));
+				
+			}
+
 			return $result;
 		}
 	}
